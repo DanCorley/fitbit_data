adfd64f608db84777dff1324af9d32ea131391b0
Dan Corley <dcorle2@gmail.com>
2020-04-22 23:44:55 -0500

    add Oauth2 to streamlit

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,11 +2,13 @@ import os, json
 import pandas as pd
 import streamlit as st
 import numpy as np
+import gather_keys_oauth2 as Oauth2
+import fitbit
+import time
 
 '''
 ## FitBit Data
-This is real-world data for myself.\n
-Select from the data values, and frequency. You may also change the time period by clicking the arrow in the top-left corner.
+Click the dropdown below to view real-world data for myself. To authorize FitBit to use your own data, open the sidebar in the top-left of the screen.\n
 '''
 
 @st.cache(suppress_st_warning=True, show_spinner=False)
@@ -87,47 +89,82 @@ def load_data(string, list_dir = '../DanielCorley/user-site-export', save=False)
         
         return df
         
-        
-value = st.selectbox('what data would you like to look at?', ['Select Data:','steps', 'distance', 'heart_rate', 'sleep', 'est_oxygen'])
 
-if value != 'Select Data:':
-    
-    df = load_data(value).fillna(method='bfill')
-    
-    min_date = st.sidebar.date_input('min date', df.index.min())
-    max_date = st.sidebar.date_input('max date', df.index.max())
+def authenticate():
 
-    df_size = df.loc[f'{min_date}':f'{max_date}'].shape
+    CLIENT_ID = '22BPLG'
+    CLIENT_SECRET = '37fd8a47808f556d72907cd18d06aaa7'
+
+    server = Oauth2.OAuth2Server(CLIENT_ID, CLIENT_SECRET,
+                                 redirect_uri='http://127.0.0.1:8080')
+
+    server.browser_authorize()
+
+    ACCESS_TOKEN = str(server.fitbit.client.session.token['access_token'])
+
+    REFRESH_TOKEN = str(server.fitbit.client.session.token['refresh_token'])
+
+    auth2_client=fitbit.Fitbit(
+        CLIENT_ID,CLIENT_SECRET,
+        oauth2=True,access_token=ACCESS_TOKEN,
+        refresh_token=REFRESH_TOKEN
+    ) 
+    return auth2_client
     
-    resamp = st.selectbox('how do you want to see the data?', ['daily','weekly'])
-    df = df.loc[f'{min_date}':f'{max_date}']
-    
-    rolling = st.checkbox('rolling avg?')
-    
-    if value in ['heart_rate','sleep']:
-        if resamp == 'weekly':
-            df = df.resample('w').mean()
-            average = df.iloc[:,0].mean()
-        else:
-            average = df.iloc[:,0].mean()
-    else:
-        if resamp == 'weekly':
-            df = df.resample('w').sum()
-            average = df.iloc[:,0].mean()
-        else:
-            average = df.iloc[:,0].mean()
-    
-    if rolling:
-        if resamp == 'weekly':
-            df['rolling_week'] = df.rolling(4).mean()
-        else:
-            df['rolling'] = df.rolling(30).mean()
-    
-    if value == 'sleep':
-        'This may look like I don\'t sleep alot, but FitBit has a "sensitive" sleep setting, which is the lower. More on that [here](https://community.fitbit.com/t5/Sleep-Better/Inaccurate-Sleep-Log-Change-your-settings/td-p/1406238)'
-    
-    st.write(f'average {resamp} {value}: {int(average):,d}'.replace('_',' '))
+personal = st.sidebar.checkbox('use personal data')
+if personal:
+    auth2_client = authenticate()
+    df = pd.DataFrame(auth2_client.time_series('activities/steps', period = 'max')['activities-steps'])
+    df.dateTime = pd.to_datetime(df.dateTime)
+    df.value = df.value.astype('int32')
+    df.set_index('dateTime', inplace=True)
     st.line_chart(df)
+        
+else:
     
-    if st.checkbox('show data'):
-        st.write(df)
+    '''
+    Select from the data values, and frequency. You may also change the time period by clicking the arrow in the top-left corner.
+    '''
+    value = st.selectbox('what data would you like to look at?', ['Select Data:','steps', 'distance', 'heart_rate', 'sleep', 'est_oxygen'])
+
+    if value != 'Select Data:':
+
+        df = load_data(value).fillna(method='bfill')
+
+        min_date = st.sidebar.date_input('min date', df.index.min())
+        max_date = st.sidebar.date_input('max date', df.index.max())
+
+        df_size = df.loc[f'{min_date}':f'{max_date}'].shape
+
+        resamp = st.selectbox('how do you want to see the data?', ['daily','weekly'])
+        df = df.loc[f'{min_date}':f'{max_date}']
+
+        rolling = st.checkbox('rolling avg?')
+
+        if value in ['heart_rate','sleep']:
+            if resamp == 'weekly':
+                df = df.resample('w').mean()
+                average = df.iloc[:,0].mean()
+            else:
+                average = df.iloc[:,0].mean()
+        else:
+            if resamp == 'weekly':
+                df = df.resample('w').sum()
+                average = df.iloc[:,0].mean()
+            else:
+                average = df.iloc[:,0].mean()
+
+        if rolling:
+            if resamp == 'weekly':
+                df['rolling_week'] = df.rolling(4).mean()
+            else:
+                df['rolling'] = df.rolling(30).mean()
+
+        if value == 'sleep':
+            'This may look like I don\'t sleep alot, but FitBit has a "sensitive" sleep setting, which is the lower. More on that [here](https://community.fitbit.com/t5/Sleep-Better/Inaccurate-Sleep-Log-Change-your-settings/td-p/1406238)'
+
+        st.write(f'average {resamp} {value}: {int(average):,d}'.replace('_',' '))
+        st.line_chart(df)
+
+        if st.checkbox('show data'):
+            st.write(df)
